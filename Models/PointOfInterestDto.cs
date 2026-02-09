54d0d417467ddefcdcb704525d09596bf1023c14
brianoneill632 <brianoneill632@gmail.com>
2026-02-09 22:47:46 +0000

    creation of POIDTO, no data entered yet

---
+++ Models/PointOfInterestDto.cs
@@ -0,0 +1,21 @@
+﻿namespace CityInfo.API.Models
+{
+    public class PointOfInterestDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public int NumberOfPointsOfINterest
+        {
+            get
+            {
+                return PointOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointOfInterest { get; set; } = [];
+    }
+}
