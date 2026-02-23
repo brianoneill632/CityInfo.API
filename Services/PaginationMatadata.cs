5c353b005a94f2d6687b7c738a612205b8bb8c45
brianoneill632 <brianoneill632@gmail.com>
2026-02-23 15:50:18 +0000

    primary constructor addition

--- Services/PaginationMatadata.cs
+++ Services/PaginationMatadata.cs
@@ -1,17 +1,10 @@
 ﻿namespace CityInfo.API.Services
 {
-    public class PaginationMatadata
+    public class PaginationMatadata(int totalCount, int pageSize, int currentPage)
     {
-        public int TotalCount { get; set; }
-        public int PageSize { get; set; }
-        public int CurrentPage { get; set; }
-        public int TotalPages { get; set; }
-         public PaginationMatadata(int totalCount, int pageSize, int currentPage)
-        {
-            TotalCount = totalCount;
-            PageSize = pageSize;
-            CurrentPage = currentPage;
-            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
-        }
+        public int TotalCount { get; set; } = totalCount;
+        public int PageSize { get; set; } = pageSize;
+        public int CurrentPage { get; set; } = currentPage;
+        public int TotalPages { get; set; } = (int)Math.Ceiling(totalCount / (double)pageSize);
     }
 }
